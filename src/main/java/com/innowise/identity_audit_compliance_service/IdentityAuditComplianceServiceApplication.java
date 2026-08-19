545d37c2db176f67ac6ff2a4ae0500ef53af1693
Infalir <irvik005@gmail.com>
2026-08-19 17:45:29 +0300

    init commit

---
+++ src/main/java/com/innowise/identity_audit_compliance_service/IdentityAuditComplianceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.identity_audit_compliance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityAuditComplianceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityAuditComplianceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innowise/identity_audit_compliance_service/IdentityAuditComplianceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.identity_audit_compliance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAuditComplianceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
